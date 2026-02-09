a48536b84e36777a4d79d5c8d72006561150f5be
JoaquinLopez94 <joaquin.dam24@gmail.com>
2026-02-09 21:14:02 +0100

    estructura

---
+++ src/main/java/com/mycompany/calculadoregrupo/CalculadoreGrupo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoregrupo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class CalculadoreGrupo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
